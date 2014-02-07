589f41259b39afb4a741295e8f2ccff10eafed23
Emmanuel Ngwane <ngwanem@hotmail.com>
2014-02-07 22:21:44 +0100

    Added test projects

---
+++ solution/xcal.application.server.web.prod.test/event_unit_tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace xcal.application.server.web.prod.test
+{
+    [TestClass]
+    public class EventUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
