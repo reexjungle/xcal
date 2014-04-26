4058fd6205f3436e40eb8e45a5b24431202c6088
Emmanuel Ngwane <ngwanem@hotmail.com>
2014-04-26 18:17:42 +0200

    added some calendar request dtos

--- solution/xcal.domain/operations/calendar.request.dtos.cs
+++ solution/xcal.domain/operations/calendar.request.dtos.cs
@@ -11,7 +11,7 @@ using reexmonkey.xcal.domain.models;
 namespace reexmonkey.xcal.domain.operations
 {
     [DataContract]
-    [Route("/calendars/create")]
+    [Route("/calendars/create", "POST")]
     public class CreateCalendar: IReturn<VCALENDAR>
     {
         [DataMember]
@@ -23,5 +23,87 @@ namespace reexmonkey.xcal.domain.operations
         [DataMember]
         public string ProductId { get; set; }
 
+        public X_COMPONENT Tag { get; set; }
+
+        public List<VTIMEZONE> TimeZones { get; set; }
+
     }
+
+    [DataContract]
+    [Route("/calendars/add", "POST")]
+    public class AddCalendar: IReturnVoid
+    {
+        [DataMember]
+        public VCALENDAR Calendar { get; set; }
+    }
+
+    [DataContract]
+    [Route("/calendars/batch/add", "POST")]
+    public class AddCalendars: IReturnVoid
+    {
+        [DataMember]
+        public List<VCALENDAR> Calendars { get; set; }
+    }
+
+
+    [DataContract]
+    [Route("/calendars/update", "POST")]
+    public class UpdateCalendar: IReturn<VCALENDAR>
+    {
+        [DataMember]
+        public VCALENDAR Calendar { get; set; }
+    }
+
+
+    [DataContract]
+    [Route("/calendars/update", "POST")]
+    public class UpdateCalendars : IReturn<List<VCALENDAR>>
+    {
+        [DataMember]
+        public List<VCALENDAR> Calendars { get; set; }
+    }
+
+    [DataContract]
+    [Route("/calendars/{CalendarId}/patch", "POST")]
+    public class PatchCalendar: IReturn<VCALENDAR>
+    {
+        [DataMember]
+        public string CalendarId { get; set; }
+
+        [DataMember]
+        public string Version { get; set; }
+
+        [DataMember]
+        public CALSCALE Scale { get; set; }
+
+        [DataMember]
+        public string ProductId { get; set; }
+
+        public X_COMPONENT Tag { get; set; }
+
+        public List<VTIMEZONE> TimeZones { get; set; }
+    }
+
+    [DataContract]
+    [Route("/calendars/batch/patch", "POST")]
+    public class PatchCalendars : IReturn<List<VCALENDAR>>
+    {
+        [DataMember]
+        public List<string> CalendarIds { get; set; }
+
+        [DataMember]
+        public string Version { get; set; }
+
+        [DataMember]
+        public CALSCALE Scale { get; set; }
+
+        [DataMember]
+        public string ProductId { get; set; }
+
+        public X_COMPONENT Tag { get; set; }
+
+        public List<VTIMEZONE> TimeZones { get; set; }
+    }
+
+
 }
